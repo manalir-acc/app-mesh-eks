3b420d9f91ec0d6257b3299218c27f67833b4827
kadamd-acc <deepak.haridas.kadam@accenture.com>
2022-06-20 22:05:03 +0530

    app mesh deployment1

--- modules/eks/main.tf
+++ modules/eks/main.tf
@@ -237,6 +237,20 @@ resource "aws_iam_role_policy_attachment" "AmazonEKSVPCResourceController2" {
 /* ===========================================
 Creating IAM Role for Fargate profile AppMesh
 ==============================================*/
+resource "aws_eks_fargate_profile" "eks_appmesh_system" {
+  cluster_name           = aws_eks_cluster.eks_cluster.name
+  fargate_profile_name   = substr("${var.cluster_name}-${var.environment}-appmesh-system-profile",0,64)
+  pod_execution_role_arn = aws_iam_role.eks_appmesh_system_role.arn
+  subnet_ids             = var.private_subnets
+
+  selector {
+    namespace = "appmesh-system"
+  }
+  timeouts {
+    create   = "30m"
+    delete   = "30m"
+  }
+}
 
 resource "aws_iam_role" "eks_appmesh_system_role" {
   name = substr("${var.cluster_name}-eks_appmesh_system_role",0,64)
