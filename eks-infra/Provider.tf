e5939e7b50fdfe05b0e4d1098fa0ea749d13b298
kadamd-acc <deepak.haridas.kadam@accenture.com>
2022-06-21 14:11:48 +0530

    deploying infra

--- eks-infra/Provider.tf
+++ eks-infra/Provider.tf
@@ -15,7 +15,6 @@ locals {
   cluster_name= data.aws_eks_cluster.eks_cluster.name
 }
 
-
 provider "kubernetes" {
   host                   = data.aws_eks_cluster.eks_cluster.endpoint
   token                  = data.aws_eks_cluster_auth.aws_iam_authenticator.token
@@ -45,6 +44,7 @@ provider "helm" {
 #    }
 #  }
 #}
+
 #provider "kubernetes" {
 #  host                   = data.aws_eks_cluster.eks_cluster.endpoint
 #  cluster_ca_certificate = base64decode(data.aws_eks_cluster.eks_cluster.certificate_authority[0].data)
