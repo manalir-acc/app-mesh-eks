03e65a1acf153a771bdbd4bd5ce71697dfa41ea8
kadamd-acc <deepak.haridas.kadam@accenture.com>
2022-07-04 09:59:12 +0530

    updating acm

--- modules/acm-cert/main.tf
+++ modules/acm-cert/main.tf
@@ -1,9 +1,3 @@
-
-data "aws_route53_zone" "zone" {
-  name         = var.hosted_zone_name
-  private_zone = "false"
-}
-
 resource "aws_acm_certificate" "this" {
   domain_name       = var.domain_name
   validation_method = "DNS"
